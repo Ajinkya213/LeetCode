432e87f9e38eb20da012397c906f91b0698f4c42
Ajinkya Bhushan <ajinkyabhushan213@gmail.com>
2024-11-14 12:21:17 +0530

    2nd correct solution but needs faster approach.

--- Easy/#922 Sort Array By Parity II.java
+++ Easy/#922 Sort Array By Parity II.java
@@ -1,36 +1,25 @@
 class Solution {
     public int[] sortArrayByParityII(int[] nums) {
-    /* Accidentally created the in - place sorting of the odd and even number
-        Funny thing is that it don't prioritise the first element should be even.
-        If the first element is odd then next will be even and vice versa
-        Nice accidents..
-    */
 
- //Intuition:
-// If we encounter an even number the next number should be odd and vice versa
-        for(int i = 0; i < nums.length - 1; i++){
-            //Either the first num is odd or even;
-            if (nums[i] % 2 == 0 ){ //checking if the first value is even if yes |V|
-                if(nums[i + 1] % 2 != 1){   //if the next value is even |V|
-                    int j = i+1;            // setting 'j'
-                    while(j < nums.length && nums[j] % 2 == 0){
-                        j++;
+        /*
+        * Completed the challenge by doing the in place sorting
+        * Time complexity O(n^2);
+        * Space complexity O(1);
+        * Time taken 6 ms.
+        * Need to find a better way.
+        * */
+        for(int i = 0 ; i < nums.length - 1; i++){
+            int index =  i % 2;
+            if(nums[i] % 2 != index){
+                int j = i+1;
+                while(j < nums.length){
+                    if(nums[j] % 2 == index){
+                        int temp = nums[i];
+                        nums[i] = nums[j];
+                        nums[j] = temp;
+                        break;
                     }
-                    int temp = nums[j];
-                    nums[j] = nums[i+1];
-                    nums[i+1] = temp;
-                    i++;
-                }
-            }else{
-                if(nums[i + 1] % 2 != 0){
-                    int j = i+1;            // setting 'j'
-                    while(j < nums.length && nums[j] % 2 == 1){
-                        j++;
-                    }
-                    int temp = nums[j];
-                    nums[j] = nums[i+1];
-                    nums[i+1] = temp;
-                    i++;
+                    j++;
                 }
             }
         }
