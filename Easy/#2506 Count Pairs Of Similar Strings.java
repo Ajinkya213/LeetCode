f06ca4a012c70865b0042bd627008d8db28007a8
Ajinkya Bhushan <ajinkyabhushan213@gmail.com>
2024-11-21 23:51:32 +0530

    #2506 optimise solution

--- Easy/#2506 Count Pairs Of Similar Strings.java
+++ Easy/#2506 Count Pairs Of Similar Strings.java
@@ -1,31 +1,29 @@
 class Solution {
     public int similarPairs(String[] words) {
-        //1. Santize the strings in the 'words' array
-        List <String> list = new ArrayList<>();
-
-        for(String word : words){
-            String str = "";
-            for(int i = 0 ; i < word.length(); i++){
-                if(!str.contains(word.substring(i, i+1))){
-                    str+=word.substring(i, i+1);
-                }
-            }
-            char tokens [] = str.toCharArray();
-            Arrays.sort(tokens);
-            str = String.valueOf(tokens);
-
-            list.add(str);
+        int pairs = 0;
+        int n = words.length;
+        int[] masks = new int[n];
+        for (int i = 0; i < n; i++) {
+            masks[i] = getMask(words[i]);
         }
 
-        int count = 0;
-        for(int i = 0 ; i < list.size() - 1; i++){
-            for(int j = i + 1; j  < list.size(); j++){
-                if(list.get(i).equals(list.get(j))){
-                    count++;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (masks[i] == masks[j]) {
+                    pairs++;
                 }
             }
         }
 
-        return count;
+        return pairs;
     }
-}
+
+    private int getMask(String s) {
+        int mask = 0;
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            mask |= (1 << s.charAt(i) - 'a');
+        }
+
+        return mask;
+    }}
