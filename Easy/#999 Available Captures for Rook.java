35f6c64931fe3731d62af1cc10df0c5182e329b3
Ajinkya Bhushan <ajinkyabhushan213@gmail.com>
2024-11-27 23:29:39 +0530

    #999 optimised solution

--- Easy/#999 Available Captures for Rook.java
+++ Easy/#999 Available Captures for Rook.java
@@ -1,64 +1,43 @@
 class Solution {
     public int numRookCaptures(char[][] board) {
-        int x = 0;
-        int y = 0;
-        boolean found = false;
+        int x = 0, y = 0;
+
+        // Find the rook's position
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
                 if (board[i][j] == 'R') {
                     x = i;
                     y = j;
-                    found = true;
                     break;
                 }
-            }
-            if (found) {
-                break;
             }
         }
 
         int capture = 0;
-        // Looking at the top
-        for (int i = x - 1; i >= 0; i--) {
-            char c = board[i][y];
-            if (Character.isLetter(c)) {
-                if (c == 'p') {
-                    capture++;
-                }
-                break;
-            }
-        }
 
-        // Looking at the bottom
-        for (int i = x + 1; i < board.length; i++) {
-            char c = board[i][y];
-            if (Character.isLetter(c)) {
-                if (c == 'p') {
-                    capture++;
-                }
-                break;
-            }
-        }
+        // Directions: top, bottom, right, left
+        int[][] directions = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
 
-        // Looking at the right
-        for (int j = y + 1; j < board[0].length; j++) {
-            char c = board[x][j];
-            if (Character.isLetter(c)) {
-                if (c == 'p') {
-                    capture++;
-                }
-                break;
-            }
-        }
+        for (int[] dir : directions) {
+            int nx = x + dir[0];
+            int ny = y + dir[1];
 
-        // Looking at the left
-        for (int j = y - 1; j >= 0; j--) {
-            char c = board[x][j];
-            if (Character.isLetter(c)) {
-                if (c == 'p') {
-                    capture++;
+            // Move in the current direction
+            while (nx >= 0 && nx < board.length && ny >= 0 && ny < board[0].length) {
+                char c = board[nx][ny];
+
+                // If we encounter a piece
+                if (Character.isLetter(c)) {
+                    // If it's a pawn, increment capture and stop
+                    if (c == 'p') {
+                        capture++;
+                    }
+                    break;
                 }
-                break;
+
+                // Move to next square in the direction
+                nx += dir[0];
+                ny += dir[1];
             }
         }
 
