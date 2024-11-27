5cb7823ea8cd952b260074dd3f2028dcc9df14c1
Ajinkya Bhushan <ajinkyabhushan213@gmail.com>
2024-11-27 23:21:58 +0530

    #1200 optimised solution

--- Easy/#1200 Minimum Absolute Difference.java
+++ Easy/#1200 Minimum Absolute Difference.java
@@ -1,27 +1,22 @@
 class Solution {
     public List<List<Integer>> minimumAbsDifference(int[] arr) {
-        List<List<Integer>> list = new ArrayList<List<Integer>>();
-        int diff [] = new int [arr.length -1];
 
         Arrays.sort(arr);
 
-        int minAbosuluteDiffrence = Integer.MAX_VALUE;
 
-        for(int i = 0; i < arr.length -1; i++){
-            diff [i] = Math.abs(arr[i+1] - arr[i]);
-            minAbosuluteDiffrence = Math.min(minAbosuluteDiffrence, diff [i]);
-            System.out.println(diff [i]);
+        int minAbsDifference = Integer.MAX_VALUE;
+        for (int i = 1; i < arr.length; i++) {
+            minAbsDifference = Math.min(minAbsDifference, arr[i] - arr[i-1]);
         }
 
-        for(int i = 0; i < diff.length; i++){
-            if(minAbosuluteDiffrence == diff[i]){
-                List<Integer> sect = new ArrayList<>();
-                sect.add(arr[i]);
-                sect.add(arr[i+1]);
-                list.add(sect);
+
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] - arr[i-1] == minAbsDifference) {
+                result.add(Arrays.asList(arr[i-1], arr[i]));
             }
         }
 
-        return list;
+        return result;
     }
 }
