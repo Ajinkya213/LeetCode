8739fde0ee109f65450d211441ad36def194a86b
Ajinkya Bhushan <ajinkyabhushan213@gmail.com>
2024-12-02 23:46:20 +0530

    #1346 set solution

--- Easy/#1346 Check If N and Its Double Exist.java
+++ Easy/#1346 Check If N and Its Double Exist.java
@@ -1,14 +1,20 @@
 class Solution {
+
     public boolean checkIfExist(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length; j++) {
-                if (i != j) {
-                    if (arr[i] == (arr[j] * 2)) {
-                        return true;
-                    }
-                }
+        Set<Integer> seen = new HashSet<>();
+
+        for (int num : arr) {
+
+            if (
+                    seen.contains(2 * num) ||
+                            (num % 2 == 0 && seen.contains(num / 2))
+            ) {
+                return true;
             }
+
+            seen.add(num);
         }
+
         return false;
     }
 }
