5d0fb29be4f7b3177df842b6df6c9c8a497ace41
Ajinkya Bhushan <ajinkyabhushan213@gmail.com>
2024-12-05 23:06:19 +0530

    #2760 solution

---
+++ Easy/#2706 Buy Two Chocolates.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int buyChoco(int[] prices, int money) {
+        Arrays.sort(prices);
+        int sum = prices[0] + prices[1];
+        if ((money - sum) < 0){
+            return money;
+        }else{
+            return Math.abs(sum - money);
+        }
+    }
+}
