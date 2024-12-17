fb012055ce2c2e61b0ba5ba9b295152db519013c
Ajinkya Bhushan <ajinkyabhushan213@gmail.com>
2024-12-17 21:24:17 +0530

    #2544 optimal solution

--- Easy/#2544 Alternating Digit Sum.java
+++ Easy/#2544 Alternating Digit Sum.java
@@ -1,16 +1,10 @@
 class Solution {
     public int alternateDigitSum(int n) {
-        String f = ""+n;
-        int sum = 0;
-        for(int i = 0; i < f.length(); i++){
-            int k = 0;
-            if(i % 2 == 0){
-                k = f.charAt(i) - '0';
-            }else{
-                k = f.charAt(i) - '0';
-                k = -k;
-            }
-            sum += k;
+        int sum=0;
+        int sign=1;
+        while(n>0){
+            sum=n%10 -sum;
+            n=n/10;
         }
         return sum;
     }
