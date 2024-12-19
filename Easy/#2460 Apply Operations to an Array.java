04a3a7c56c89e95243feda3d5a59b2949c1500c9
Ajinkya Bhushan <ajinkyabhushan213@gmail.com>
2024-12-19 23:08:36 +0530

    #2460 optimised solution 3

--- Easy/#2460 Apply Operations to an Array.java
+++ Easy/#2460 Apply Operations to an Array.java
@@ -1,23 +1,23 @@
 class Solution {
     public int[] applyOperations(int[] nums) {
-        for(int i = 0; i < nums.length - 1 ; i++){
-            if(nums[i] == nums[i+1]){
-                nums[i] *= 2;
-                nums[i+1] = 0;
+        int[] ans = new int[nums.length];
+        int j = 0;
+        int i = 0;
+        for(; i < nums.length - 1; i++)
+        {
+            if (nums[i] == 0) continue;
+            if (nums[i] == nums[i + 1])
+            {
+                ans[j++] = 2 * nums[i];
+                nums[i+ 1] = 0;
+            }else
+            {
+                ans[j++] = nums[i];
             }
         }
 
-        int index  = 0;
-        for(int i = 0; i < nums.length ; i++){
-            if(nums[i] != 0){
-                nums[index] = nums[i];
-                index++;
-            }
-        }
+        ans[j] = nums[i];
 
-        for(int i = index; i < nums.length; i++){
-            nums[i] = 0;
-        }
-        return nums;
+        return ans;
     }
 }
