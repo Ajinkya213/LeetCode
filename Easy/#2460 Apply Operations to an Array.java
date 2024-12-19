591fabd9133b6b3ced3c28cf86cf11e5b5203d7f
Ajinkya Bhushan <ajinkyabhushan213@gmail.com>
2024-12-19 23:08:07 +0530

    #2460 optimised solution 2

--- Easy/#2460 Apply Operations to an Array.java
+++ Easy/#2460 Apply Operations to an Array.java
@@ -7,20 +7,16 @@ class Solution {
             }
         }
 
-        //Shifting zeros
-        for(int i = 0; i < nums.length -1 ; i++){
-            if(nums[i] == 0){
-                int j = i+1;
-                while(j < nums.length){
-                    if(nums[j] > 0){
-                        int temp = nums[i];
-                        nums[i] = nums[j];
-                        nums[j] = temp;
-                        break;
-                    }
-                    j++;
-                }
+        int index  = 0;
+        for(int i = 0; i < nums.length ; i++){
+            if(nums[i] != 0){
+                nums[index] = nums[i];
+                index++;
             }
+        }
+
+        for(int i = index; i < nums.length; i++){
+            nums[i] = 0;
         }
         return nums;
     }
