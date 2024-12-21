e2f9d52187a84eb3e2f0f282fdc4618367427de8
Ajinkya Bhushan <ajinkyabhushan213@gmail.com>
2024-12-22 00:35:03 +0530

    #1544 optimal solution 3

--- Easy/#1544 Make The String Great.java
+++ Easy/#1544 Make The String Great.java
@@ -1,20 +1,16 @@
 class Solution {
     public String makeGood(String s) {
-        Stack<Character> stack = new Stack<>();
-
+        StringBuilder sb = new StringBuilder();
         for (char c : s.toCharArray()) {
-            if (!stack.isEmpty() && Math.abs(c - stack.peek()) == 32) {
-                stack.pop();
+            if (sb.length() > 0 && Math.abs(sb.charAt(sb.length() - 1) - c) == 32) {
+
+                sb.deleteCharAt(sb.length() - 1);
             } else {
-                stack.push(c);
+
+                sb.append(c);
             }
         }
-
-        StringBuilder result = new StringBuilder();
-        while (!stack.isEmpty()) {
-            result.insert(0, stack.pop());
-        }
-
-        return result.toString();
+        return sb.toString();
     }
+
 }
