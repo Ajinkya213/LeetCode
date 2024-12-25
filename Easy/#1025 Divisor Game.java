e15eab2ff1a6da15a0d5e4fae79d9d63bc32db0a
Ajinkya Bhushan <ajinkyabhushan213@gmail.com>
2024-12-25 18:42:12 +0530

    #1025 solution 3

--- Easy/#1025 Divisor Game.java
+++ Easy/#1025 Divisor Game.java
@@ -1,17 +1,5 @@
 class Solution {
     public boolean divisorGame(int n) {
-        //Simulation
-        boolean aliceWins = false;
-        while(n != 0){
-            if(n % 1 == 0){
-                n -= 1;
-                if(!aliceWins){
-                    aliceWins = true;
-                }else{
-                    aliceWins = false;
-                }
-            }
-        }
-        return !aliceWins;
+        return (n & 1) == 0;
     }
 }
