6ba0c30fce29e1fe1df33b32f45b603e011321ec
Ajinkya Bhushan <ajinkyabhushan213@gmail.com>
2025-01-01 00:18:10 +0530

    #2639 optimal solution

--- Easy/#2639 Find the Width of Columns of a Grid.java
+++ Easy/#2639 Find the Width of Columns of a Grid.java
@@ -1,16 +1,23 @@
 class Solution {
     public int[] findColumnWidth(int[][] grid) {
         int len = grid.length;
-        int maxColWidth [] = new int [grid[0].length];
-        int k = 0;
-        for(int i = 0; i < grid[0].length; i++){
-            int max = -1;
-            for(int j = 0; j < len ; j++){
-                String s = grid[j][i]+" ";
-                max = Math.max(max , s.length() - 1);
+        int len1 = grid[0].length;
+        int ans[] = new int[len1];
+        for(int i = 0; i < len1; i++)
+        {
+            long ma = 0;
+            for(int j = 0; j < len;j++)
+            {
+                long c = grid[j][i];
+                if(c < 0)
+                    c *= -10;
+                if(c > ma)
+                    ma = c;
             }
-            maxColWidth[i] = max;
+            if(ma > 0)
+                ans[i] = (int)(Math.log10(ma));
+            ans[i]++;
         }
-        return maxColWidth;
+        return ans;
     }
 }
