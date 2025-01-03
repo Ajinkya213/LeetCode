6dce2e067f4d6511f27b5dfe25080e0320a2b77b
Ajinkya Bhushan <ajinkyabhushan213@gmail.com>
2025-01-03 23:31:52 +0530

    #2270 optimal solution

--- Easy/#2270 Number of Ways to Split Array.java
+++ Easy/#2270 Number of Ways to Split Array.java
@@ -1,24 +1,22 @@
 class Solution {
 
     public int waysToSplitArray(int[] nums) {
-        int n = nums.length;
-        // Prefix sum array to store cumulative sums
-        long[] prefSum = new long[n];
-        prefSum[0] = nums[0];
+        // Keep track of sum of elements on left and right sides
+        long leftSum = 0, rightSum = 0;
 
-        // Build prefix sum array
-        for (int i = 1; i < n; i++) {
-            prefSum[i] = prefSum[i - 1] + nums[i];
+        // Initially all elements are on right side
+        for (int num : nums) {
+            rightSum += num;
         }
 
         int count = 0;
-        // Check each possible split position
-        for (int i = 0; i < n - 1; i++) {
-            // Sum of left part (0 to i)
-            long leftSum = prefSum[i];
-            // Sum of right part (i+1 to n-1)
-            long rightSum = prefSum[n - 1] - prefSum[i];
+        // Try each possible split position
+        for (int i = 0; i < nums.length - 1; i++) {
+            // Move current element from right to left side
+            leftSum += nums[i];
+            rightSum -= nums[i];
 
+            // Check if this creates a valid split
             if (leftSum >= rightSum) {
                 count++;
             }
