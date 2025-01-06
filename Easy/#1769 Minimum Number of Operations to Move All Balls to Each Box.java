eff2cefaeaa2b7b03885c5473e0218547181dc82
Ajinkya Bhushan <ajinkyabhushan213@gmail.com>
2025-01-06 23:11:22 +0530

    #1769 better solution

--- Easy/#1769 Minimum Number of Operations to Move All Balls to Each Box.java
+++ Easy/#1769 Minimum Number of Operations to Move All Balls to Each Box.java
@@ -1,27 +1,18 @@
 class Solution {
+
     public int[] minOperations(String boxes) {
-        int [] minOps = new int [boxes.length()];
-
-        for(int i = 0; i < boxes.length(); i++){
-            //Front side ->
-            int hops = 0;
-            for(int j = 0; j < i; j++){
-                if(boxes.charAt(j) == '1'){
-                    hops+= Math.abs(i - j);
+        int[] answer = new int[boxes.length()];
+        for (int currentBox = 0; currentBox < boxes.length(); currentBox++) {
+            if (boxes.charAt(currentBox) == '1') {
+                for (
+                        int newPosition = 0;
+                        newPosition < boxes.length();
+                        newPosition++
+                ) {
+                    answer[newPosition] += Math.abs(newPosition - currentBox);
                 }
             }
-
-            // <-Back side
-
-            for(int j = i; j < boxes.length(); j++){
-                if(boxes.charAt(j) == '1'){
-                    hops+= Math.abs(i - j);
-                }
-            }
-
-            minOps[i] = hops;
         }
-
-        return minOps;
+        return answer;
     }
 }
