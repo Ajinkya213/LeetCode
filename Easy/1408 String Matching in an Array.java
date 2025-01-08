a6b06fed73b8b4f46970988e3154a70a46949bd5
Ajinkya Bhushan <ajinkyabhushan213@gmail.com>
2025-01-08 12:38:03 +0530

    #1408 a bit better solution

--- Easy/1408 String Matching in an Array.java
+++ Easy/1408 String Matching in an Array.java
@@ -1,17 +1,16 @@
 class Solution {
     public List<String> stringMatching(String[] words) {
-        List <String> matchedStrings = new ArrayList <>();
-        for(int i = 0; i < words.length; i++){
-            for(int j = 0; j < words.length; j++){
-                if(i != j){
-                    if(words[i].contains(words[j])){
-                        if(!matchedStrings.contains(words[j])){
-                            matchedStrings.add(words[j]);
-                        }
-                    }
+        List<String> result = new ArrayList<>();
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+            for (int j = 0; j < words.length; j++) {
+                if (i == j) continue;
+                if (words[j].indexOf(word) >= 0) {
+                    result.add(word);
+                    break;
                 }
             }
         }
-        return matchedStrings;
+        return result;
     }
 }
