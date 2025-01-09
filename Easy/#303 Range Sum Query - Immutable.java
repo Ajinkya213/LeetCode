d68402d1d79df925d85e19c51f5c99201c6a1cbb
Ajinkya Bhushan <ajinkyabhushan213@gmail.com>
2025-01-09 22:49:14 +0530

    #303 best solution solved with prefixsum

--- Easy/#303 Range Sum Query - Immutable.java
+++ Easy/#303 Range Sum Query - Immutable.java
@@ -1,15 +1,22 @@
 class NumArray {
-    int numsArray [];
+    int sums [];
+    List<Integer> list = new ArrayList<>();
     public NumArray(int[] nums) {
-        this.numsArray = nums;
+        this.sums = new int [nums.length];
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++){
+            sum += nums[i];
+            this.sums[i] = sum;
+        }
+
     }
 
     public int sumRange(int left, int right) {
-        int sum = 0;
-        for(int index = left; index <= right; index++){
-            sum += numsArray[index];
+        if (left == 0){
+            return this.sums[right];
+        }else{
+            return this.sums[right] - this.sums[left-1];
         }
-        return sum;
     }
 }
 
