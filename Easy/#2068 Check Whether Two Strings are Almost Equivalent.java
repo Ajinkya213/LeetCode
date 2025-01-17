f5bcdfad450c90d50e81303fb23d2562f19450b3
Ajinkya Bhushan <ajinkyabhushan213@gmail.com>
2025-01-18 02:15:58 +0530

    #2068 using single charMap

--- Easy/#2068 Check Whether Two Strings are Almost Equivalent.java
+++ Easy/#2068 Check Whether Two Strings are Almost Equivalent.java
@@ -1,18 +1,17 @@
 class Solution {
     public boolean checkAlmostEquivalent(String word1, String word2) {
-        int word1Map [] = new int [26];
-        int word2Map [] = new int [26];
+        int wordMap [] = new int [26];
 
         for(char ch : word1.toCharArray()){
-            word1Map[ch-'a']++;
+            wordMap[ch-'a']++;
         }
 
         for(char ch : word2.toCharArray()){
-            word2Map[ch-'a']++;
+            wordMap[ch-'a']--;
         }
 
         for(int i = 0; i < 26; i++){
-            if(Math.abs(word1Map[i] - word2Map[i]) > 3){
+            if(Math.abs(wordMap[i]) > 3){
                 return false;
             }
         }
